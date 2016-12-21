da0ca373f7ebae7591522439a36e26655c123734
John Belamaric <jbelamaric@infoblox.com>
2016-12-21 18:59:38 +0000

    Minor cleanup per review

--- middleware/grpc/setup.go
+++ middleware/grpc/setup.go
@@ -41,31 +41,27 @@ func grpcParse(c *caddy.Controller) (*grpc, error) {
 	g := &grpc{config: config}
 
 	for c.Next() {
-		if c.Val() == "grpc" {
-			addr := c.RemainingArgs()
+		addr := c.RemainingArgs()
 
-			if len(addr) == 0 {
-				g.addr = ""
-			} else {
-				g.addr = addr[0]
-			}
-
-			for c.NextBlock() {
-				switch c.Val() {
-				case "tls": // cert key cacertfile
-					args := c.RemainingArgs()
-					if len(args) != 3 {
-						return nil, c.ArgErr()
-					}
-					tls, err := tls.NewTLSConfig(args[0], args[1], args[2])
-					if err != nil {
-						return nil, c.ArgErr()
-					}
-					g.tls = tls
-				}
-			}
-			return g, nil
+		if len(addr) > 0 {
+			g.addr = addr[0]
 		}
+
+		for c.NextBlock() {
+			switch c.Val() {
+			case "tls": // cert key cacertfile
+				args := c.RemainingArgs()
+				if len(args) != 3 {
+					return nil, c.ArgErr()
+				}
+				tls, err := tls.NewTLSConfig(args[0], args[1], args[2])
+				if err != nil {
+					return nil, c.ArgErr()
+				}
+				g.tls = tls
+			}
+		}
+		return g, nil
 	}
 	return nil, errors.New("grpc setup called without keyword 'grpc' in Corefile")
 }
