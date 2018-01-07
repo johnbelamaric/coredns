72d469f4d7ba0ca43c1e50d07ca3f9e377e9cae8
John Belamaric <jbelamaric@infoblox.com>
2018-01-07 13:51:27 -0500

    Fix fall_test

--- plugin/pkg/fall/fall_test.go
+++ plugin/pkg/fall/fall_test.go
@@ -2,41 +2,39 @@ package fall
 
 import "testing"
 
-func TestIsNil(t *testing.T) {
-	var f *F
-	if !f.IsNil() {
-		t.Errorf("F should be nil")
-	}
-}
-
-func TestIsZero(t *testing.T) {
-	f := New()
-	if !f.IsZero() {
+func TestZero(t *testing.T) {
+	var f F
+	if !f.Equal(Zero()) {
 		t.Errorf("F should be zero")
 	}
 }
 
-func TestFallThroughExample(t *testing.T) {
-	if !Example.Through("example.org.") {
-		t.Errorf("example.org. should fall through")
+func TestSetZonesFromArgs(t *testing.T) {
+	var f F
+	f.SetZonesFromArgs([]string{})
+	if !f.Equal(Root()) {
+		t.Errorf("F should have the root zone")
 	}
-	if Example.Through("example.net.") {
-		t.Errorf("example.net. should not fall through")
+
+	f.SetZonesFromArgs([]string{"example.com", "example.net."})
+	expected := F{Zones: []string{"example.com.", "example.net."}}
+	if !f.Equal(expected) {
+		t.Errorf("F should be %v but is %v", expected, f)
 	}
 }
 
 func TestFallthrough(t *testing.T) {
-	var fall *F
+	var fall F
 	if fall.Through("foo.com.") {
-		t.Errorf("Expected false, got true for nil fallthrough")
+		t.Errorf("Expected false, got true for zero fallthrough")
 	}
 
-	fall = New()
+	fall.SetZonesFromArgs([]string{})
 	if !fall.Through("foo.net.") {
 		t.Errorf("Expected true, got false for all zone fallthrough")
 	}
 
-	fall.SetZones([]string{"foo.com", "bar.com"})
+	fall.SetZonesFromArgs([]string{"foo.com", "bar.com"})
 
 	if fall.Through("foo.net.") {
 		t.Errorf("Expected false, got true for non-matching fallthrough zone")
