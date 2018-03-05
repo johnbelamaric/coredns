352225ac164c604a0475a5864510d88e04f51f27
Chris O'Haver <cohaver@infoblox.com>
2018-03-05 17:48:25 -0500

    document exposed funcs

--- plugin/kubernetes/kubernetes.go
+++ plugin/kubernetes/kubernetes.go
@@ -302,10 +302,12 @@ func (k *Kubernetes) Records(state request.Request, exact bool) ([]msg.Service, 
 	return services, err
 }
 
+// ServiceFQDN returns the k8s cluster dns spec service FQDN for the service (or endpoint) object
 func ServiceFQDN(obj meta.Object, zone string) string {
 	return dnsutil.Join(append([]string{}, obj.GetName(), obj.GetNamespace(), Svc, zone))
 }
 
+// EndpointFQDN returns a list of k8s cluster dns spec service FQDNs for each subset in the endpoint
 func EndpointFQDN(ep *api.Endpoints, zone string, endpointNameMode bool) []string {
 	var names []string
 	for _, ss := range ep.Subsets{
